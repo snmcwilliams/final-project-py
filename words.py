c2516f1408f289a8b4e63c2b85e58864bcf84bee
Sarah McWilliams <snmcwilliams@gmail.com>
2020-11-12 11:22:10 -0500

    List of random words

---
+++ words.py
@@ -0,0 +1,18 @@
+word_list = [
+    'sunshine',
+    'flower',
+    'rainbow',
+    'treehouse',
+    'bluebird',
+    'mountain',
+    'rain',
+    'breezy',
+    'snowman',
+    'beach',
+    'seagull',
+    'pineapple',
+    'cloudy',
+    'trail',
+    'rattlesnake',
+    'dolpin'
+    ]
