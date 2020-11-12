42b01531726ceea2b0f8e53fa3b4e2b45a79809e
Sarah McWilliams <snmcwilliams@gmail.com>
2020-11-12 12:08:26 -0500

    Added function to get random word

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 # get word list and make random
 import random
 from words import word_list
+
 # function to return word for game
 def get_word():
+    word = random.choice(word_list) # select random word from word list
+    return word.upper() # upper for uniformity and comparison
+
+# funcion to play the game
