c7aa4607fb039b24ebce497b54b9c1c91e8b46be
Sarah McWilliams <snmcwilliams@gmail.com>
2020-11-12 19:52:57 -0500

    Continue loop

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ def get_word():
     word = random.choice(word_list)
     return word.upper() # upper for uniformity and comparison
 
-# play the game
+# the game
 def gameplay(word):
     word_selected = "_" * len(word) # place holder for each letter
     guessed = False
@@ -21,7 +21,7 @@ def gameplay(word):
 # run until word is guessed or runs out of attempts
     while not guessed and tries > 0:
         guess = input("Guess a letter or word: ").upper()
- # conditions for guessing letter - duplicate, not found and is found.
+    # conditions for guessing letter
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
                 print("Oops, you've already guessed the letter", guess)
@@ -32,17 +32,17 @@ def gameplay(word):
             else:
                 print("Well done, ", guess, " is in the word!")
                 guessed_letters.append(guess)
-# display guess occurrences
+    # display guess occurrences
                 word_as_list = list(word_selected) # string to list for index
-            # find indices where guess occurs in word
+                # find indices where guess occurs in word
                 indices = [i for i, letter in enumerate(word) if letter == guess] # get index and letter at index for each iteration
-            # replace underscore at index with guess
+                # replace underscore at index with guess
                 for index in indices:
                     word_as_list[index] = guess
                 word_selected = "".join(word_as_list) # list to string
                 if "_" not in word_selected:
                     guessed = True
-# conditions for guessing word
+    # conditions for guessing word
         elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
                 print("Oops, you've already guessed the word", guess)
@@ -53,7 +53,7 @@ def gameplay(word):
             else:
                 guessed = True
                 word_selection = word
-# conditions for guessing other
+    # conditions for guessing other
         else:
             print("Invalid guess.")
     # show after each attempt
@@ -64,8 +64,6 @@ def gameplay(word):
         print("Awesome, you guessed the word!")
     else:
         print("Darn it, you've exceeded number of attempts. The word was "+ word + ". Try Again!")
-
-
 
 
 def display_hangman(attempts):
@@ -126,3 +124,16 @@ def display_hangman(attempts):
         """
     ]
     return stages[attempts]
+
+# continue to play
+def main():
+    word = get_word
+    play(word)
+    while input("Play Again? YES OR NO").upper() == "YES":
+        word = get_word()
+        play(word)
+
+# run on command line
+if __name__ == "__main__":
+    main()
+
